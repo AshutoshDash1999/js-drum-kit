9e963b93a240d013ccd884d48a57fc3aa56ec1be
Ashutosh Dash <dashashutosh1999@gmail.com>
2021-10-08 00:01:40 +0530

    minor improvement

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # JS Drum Kit
 
 A simple drum kit built on JavaScript.
+
+Live project link [here](https://js-drumm-kitt.netlify.app/).
--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 //play sound with keyboard event
 addEventListener("keydown", function(event){
     var audioKey = document.querySelector(`audio[data-key="${event.key}"]`);
+    audioKey.currentTime = 0; //rewind sound from start
     audioKey.play();
     var audioButton = document.querySelector(`#audio-${event.key}`);
     audioButton.style.background = "#8B5CF6";
